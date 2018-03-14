2fc7c0ea2ed42746568332bd530bf5232a19637a
chewning.cl@gmail.com <chewning.cl@gmail.com>
2018-03-14 20:16:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,22 +14,7 @@ function divide(g,h) {
   return g / h; 
 }
 
-function inc(n) {
-  return n++;
-}
-
-function dec(n) {
-  return n--;
-}
 
 var n = 5;
 n++;
 
-
-function makeInteger(n){
-  return parseInt(n)
-}
-
-function parseInt() {
-  return 
-}
