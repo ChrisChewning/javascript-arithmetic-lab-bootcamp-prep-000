cbee8a8e980825ff6c0bc0c700466ec7a82a191a
chewning.cl@gmail.com <chewning.cl@gmail.com>
2018-03-14 20:28:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,12 @@ function divide(g,h) {
   return g / h; 
 }
 
+
+
+it('inc(n) increments n and returns the result', function() {
+  expect(inc(a)).toEqual(a + 1)
+})
+
+it('dec(n) decrements n and returns the result', function() {
+  expect(dec(a)).toEqual(a - 1)
+})
