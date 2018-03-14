04490feb5ca6133bdf63a5f879b5ceebe7e2dc10
chewning.cl@gmail.com <chewning.cl@gmail.com>
2018-03-14 20:32:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ it('divide(a, b) divides a by b and returns the result', function() {
   expect(divide(a, b)).toEqual(a / b)
 })
 
-var number = 5 
-number++
+var number = 5; 
+number++;
 
 it('inc(n) increments n and returns the result', function() {
   expect(inc(a)).toEqual(a + 1)
