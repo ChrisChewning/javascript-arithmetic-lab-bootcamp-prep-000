ab060de955f5cc556946e4b9e71b61c514ae8b42
chewning.cl@gmail.com <chewning.cl@gmail.com>
2018-03-14 20:43:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,15 +20,8 @@ function inc(n){
   return n;
 }
 
-var n = 0;
-function inc(n) {
-  n--;
-  return n;
-}
 
-it('inc(n) increments n and returns the result', function() {
-  expect(inc(a)).toEqual(a + 1)
-})
+
 
 it('dec(n) decrements n and returns the result', function() {
   expect(dec(a)).toEqual(a - 1)
